25bd4609ab1ca539d42c614b31ff06950b5cb152
Jeff <je1400r@gmail.com>
2025-06-04 04:16:27 -0500

    Agregado start.sh para levantar Django y FastAPI

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Levanta Django en segundo plano en el puerto 8000
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 &
+
+# Levanta FastAPI en el puerto 8001 
+uvicorn backend.api.main:app --host 0.0.0.0 --port 8001
